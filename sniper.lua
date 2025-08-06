7ce89ae572cfcc959f352a7c6d954e1676d90d90
k0nkx <140971614+k0nkx@users.noreply.github.com>
2025-08-06 08:46:12 -0700

    Create sniper.lua

---
+++ sniper.lua
@@ -0,0 +1,29 @@
+getgenv().UseScript = true
+getgenv()._=
+"Join discord.gg/msgabv2t9Q | discord.gg/stando to get latest update ok bai | If you pay for this script you get scammed, this script is completely free ok"
+
+if getgenv().UseScript then
+    getgenv().Owner = "Rias_High"
+
+    getgenv().Configuration = {
+        ['Guns'] = {"Rifle", "Flintlock"},
+        ['Strafe'] = "Random",
+        ['Void'] = "Meta",
+        ['Lerp'] = 0.1,
+        ['CFrame'] = {0, 1, 0},
+        ['Prediction'] = 1,
+        ['Multiplier'] = 3,
+        ['StompHeight'] = 4,
+        ['RandomStompOffset'] = 1.7,
+        ['SpareAmmo'] = 8,
+        ['ForceAmmo'] = 4,
+        ['CrewID'] = 32570691,
+        ['AntiStomp'] = true,
+        ['LowGraphic'] = false,
+        ['HideScreen'] = true,
+        ['CustomPrefix'] = "!",
+        ['Fpscap'] = 150
+    }
+
+    loadstring(game:HttpGet("https://xk5ng.github.io/Sniper-Bot-Rewrite"))()
+end
